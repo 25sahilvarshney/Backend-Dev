38d0bf229ba1817214d5032e125bea4ba357e2f5
SAHIL VARSHNEY <145244634+25sahilvarshney@users.noreply.github.com>
2026-01-22 20:46:53 +0530

    (sahil varshney ) Update stringUtils.js

--- CustomModule/stringUtils.js
+++ CustomModule/stringUtils.js
@@ -1,13 +1,3 @@
-/**
- * String utility functions module
- * Provides methods for string manipulation
- */
-
-/**
- * Capitalizes the first letter of a string
- * @param {string} str - The string to capitalize
- * @returns {string} The capitalized string
- */
 function capitalize(str) {
   if (typeof str !== 'string' || str.length === 0) {
     return str;
@@ -15,11 +5,6 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-/**
- * Reverses a string
- * @param {string} str - The string to reverse
- * @returns {string} The reversed string
- */
 function reverseString(str) {
   if (typeof str !== 'string') {
     return str;
@@ -27,11 +12,6 @@ function reverseString(str) {
   return str.split('').reverse().join('');
 }
 
-/**
- * Counts the number of vowels in a string
- * @param {string} str - The string to analyze
- * @returns {number} The count of vowels
- */
 function countVowels(str) {
   if (typeof str !== 'string') {
     return 0;
