a027c6068c3e9022f04189c7d7562b839f94f625
SAHIL VARSHNEY <145244634+25sahilvarshney@users.noreply.github.com>
2026-01-23 10:01:29 +0530

    Update systemLogger.js

--- SystemInfromationLogger/systemLogger.js
+++ SystemInfromationLogger/systemLogger.js
@@ -2,19 +2,19 @@ const os = require("os");
 const fs = require("fs");
 const path = require("path");
 
-const logFile = path.join(__dirname, "system-info.log");
+const logFile = path.join(dirname, "system-info.log");
 
 console.log("System Logger started...");
 
 setInterval(() => {
-  const info = `
+  const getinfo= `
 Platform: ${os.platform()}
 CPU Cores: ${os.cpus().length}
 Total Memory: ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB
 ------------------------------------
 `;
 
-  fs.appendFile(logFile, info, (err) => {
+  fs.appendFile(logFile,getinfo, (err) => {
     if (err) console.error(err);
   });
 }, 5000);
