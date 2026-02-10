a70c20ce3d893d6a2eedf23859adec58f4259981
SAHIL VARSHNEY <sahilvvs23@gmail.com>
2026-02-10 10:21:12 +0530

    new commits

--- Movie-Ticket-Booking-System/logger.js
+++
@@ -1,6 +0,0 @@
-const fs=require("fs");
-function logger(message){
-    const loadmessage=`${new Date().toString()} - ${message}\n`;
-    fs.appendFileSync("server.log",loadmessage); 
-}
-module.exports=logger;
