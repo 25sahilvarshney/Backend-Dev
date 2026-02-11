8f82f0a3f5c2a0ca18969a9dc7dabdc9ff8dddc6
SAHIL VARSHNEY <sahilvvs23@gmail.com>
2026-02-11 23:19:19 +0530

    new commits [sahil varshney]

--- assignment4/express-framework/src/about.js
+++
@@ -1,4 +0,0 @@
-function about(){
-    return "<h2>welcome to my world ! hello , my name is sahil varshney  from  uttar pradesh , india </h2>";
-}
-module.exports=about;
--- assignment4/express-framework/src/home.js
+++
@@ -1,4 +0,0 @@
-function home(){
-    return "<h1>HELLO EVERYONE </h1>";
-}
-module.exports=home
